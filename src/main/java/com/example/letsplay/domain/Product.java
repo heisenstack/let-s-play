7e1931e25f91c84a23a482ceb2f37904d6057c59
heisenstack <dinaniamine@gmail.com>
2026-02-13 00:16:50 +0100

    product entity args

--- src/main/java/com/example/letsplay/domain/Product.java
+++ src/main/java/com/example/letsplay/domain/Product.java
@@ -1,7 +1,15 @@
 package com.example.letsplay.domain;
 
+import lombok.AllArgsConstructor; 
+import lombok.Data;
+import lombok.NoArgsConstructor; 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document(collection = "products")
+@Data
+@NoArgsConstructor   
+@AllArgsConstructor  
 public class Product {
     @Id
     private String id;
