8d44954b040d2c94a8be9260f559e9e9da2664e7
heisenstack <dinaniamine@gmail.com>
2026-02-13 23:15:56 +0100

    user register

--- src/main/java/com/example/letsplay/presentation/UserController.java
+++ src/main/java/com/example/letsplay/presentation/UserController.java
@@ -1,9 +1,19 @@
 package com.example.letsplay.presentation;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import com.example.letsplay.domain.User;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+    public UserController() {
+    }
 
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User registerUser(@RequestBody RegisterRequest registerRequest) {
+        // return userService.registerUser(registerRequest);
+    }
 }
