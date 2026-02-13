aeae9707c3f753ae8a6dc5f941006e52564a04bc
heisenstack <dinaniamine@gmail.com>
2026-02-13 23:10:36 +0100

    user entity args

--- src/main/java/com/example/letsplay/domain/User.java
+++ src/main/java/com/example/letsplay/domain/User.java
@@ -1,9 +1,15 @@
 package com.example.letsplay.domain;
 
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "users")
 public class User {
 
     @Id
