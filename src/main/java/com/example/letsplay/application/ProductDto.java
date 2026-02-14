be7d14132eae737169a1c38553ca2d40d851fdbd
heisenstack <dinaniamine@gmail.com>
2026-02-14 19:50:01 +0100

    product dto

---
+++ src/main/java/com/example/letsplay/application/ProductDto.java
@@ -0,0 +1,16 @@
+package com.example.letsplay.application.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDto {
+    private String id;
+    private String name;
+    private String description;
+    private double price;
+    private String userId; 
+}
