2b3454cccdcdc3f1143ac9c47107360af7a7e632
heisenstack <dinaniamine@gmail.com>
2026-02-15 14:23:31 +0000

    user service

--- src/main/java/com/example/letsplay/presentation/UserController.java
+++ src/main/java/com/example/letsplay/presentation/UserController.java
@@ -3,12 +3,18 @@ package com.example.letsplay.presentation;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import com.example.letsplay.application.RegisterRequest;
 import com.example.letsplay.domain.User;
+import com.example.letsplay.application.UserDto;
+import com.example.letsplay.application.UserService;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
-    public UserController() {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @PostMapping("/register")
