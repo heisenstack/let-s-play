b63398ffea175f5a8c5da056a84f40bda2600cc7
heisenstack <dinaniamine@gmail.com>
2026-02-18 16:21:19 +0000

    adding user repo for jpa operations

--- src/main/java/com/example/letsplay/application/RegisterRequest.java
+++ src/main/java/com/example/letsplay/application/RegisterRequest.java
@@ -1,8 +1,12 @@
 package com.example.letsplay.application;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
+@Getter
+@Setter
 public class RegisterRequest {
     private String name;
     private String email;
--- src/main/java/com/example/letsplay/domain/User.java
+++ src/main/java/com/example/letsplay/domain/User.java
@@ -2,13 +2,18 @@ package com.example.letsplay.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 @Document(collection = "users")
 public class User {
 
