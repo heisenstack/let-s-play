2bb85ee9abb33a5eb2b7eb46921059fbba5072f2
heisenstack <dinaniamine@gmail.com>
2026-02-19 22:18:52 +0000

    adding product request/ response dto

---
+++ src/main/java/com/example/letsplay/dto/ProductRequest.java
@@ -0,0 +1,10 @@
+package com.example.letsplay.dto;
+
+import lombok.Data;
+
+@Data
+public class ProductRequest {
+    private String name;
+    private String description;
+    private Double price;
+}
