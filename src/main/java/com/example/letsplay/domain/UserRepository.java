22fc3a62e1e137c5b65edc9d1eb8b1c958f2c7a7
heisenstack <dinaniamine@gmail.com>
2026-02-19 22:07:40 +0000

    changing the structure

--- src/main/java/com/example/letsplay/domain/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.letsplay.domain;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.Optional;
-
-public interface UserRepository extends MongoRepository<User, String> {
-    boolean existsByUsername(String username);
-    User save(User user);
-    Optional<User> findByUsername(String username);
-}
